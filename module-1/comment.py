a20c87b4c1ad2340aa78779f56a247ceff766ba0
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-11-29 19:44:47 +0600

    started to learn python

---
+++ module-1/comment.py
@@ -0,0 +1,9 @@
+# this is a comment
+
+
+"""
+this is multi line comment
+
+This is multi line comment in python
+"""
+print('this is printed from python')
---
+++ module-1/hello.py
@@ -0,0 +1,30 @@
+print('Hello word')
+print('Hello python user.')
+
+# declear some variable
+
+a = 2
+b = 3
+
+# calculate the result
+
+sum = a + b
+multiply = a * b
+div = a / 2
+subs = a - b
+
+# this is a comment
+
+# print the al result
+
+print(sum)
+print(multiply)
+print(div)
+print(subs)
+
+people_name = 'something';
+
+person = [1,2,3,4]
+print(person)
+
+print(people_name)
