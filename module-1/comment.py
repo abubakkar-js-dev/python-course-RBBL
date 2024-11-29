e04ee0382020070451b9997802d17aa2bf4f582e
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-11-29 20:08:26 +0600

    commited

--- module-1/comment.py
+++ module-1/comment.py
@@ -7,3 +7,12 @@ this is multi line comment
 This is multi line comment in python
 """
 print('this is printed from python')
+
+# print a number
+
+print(543)
+
+# declear a variable
+
+friend_name = 'LoL'
+print(friend_name)
