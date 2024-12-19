e6a98efbdfd2ca9ae0dfd3ad4343534850d57b8b
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-12-19 22:21:25 +0600

    done previous day concept

---
+++ string-manipulation/string.py
@@ -0,0 +1,16 @@
+name = 'Md Abu Bakkar Siddik' # single quote string for short data.
+
+sententce = "He said to me, 'he is married'" # if have under sigle string then use double quote
+
+greet='She said, "I am busy now"' # consider
+
+pre='''
+    This is a line
+    This is another line
+    this is line under pre
+'''
+
+print(name)
+print(sententce)
+print(greet)
+print(pre)
